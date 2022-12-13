1188f0b85ae093117a572bfc9f664f6a286ff128
김경식 <envy7937@gmail.com>
2022-12-13 16:28:03 +0900

    feat(stick-study) : modify index

--- stick-study/src/App.js
+++ stick-study/src/App.js
@@ -1,21 +1,24 @@
-import logo from './logo.svg';
+import hos from './hos.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={hos} className="App-logo" alt="logo" />
+        <div>
+          <p>♚♚히어로즈 오브 더 스☆톰♚♚</p>
+          <p>가입시$$전원 카드팩☜☜뒷면100%증정※ ♜월드오브 워크래프트♜펫 무료증정￥</p>
+          <p>특정조건 §§디아블로3§§★공허의유산★초상화획득기회@@@ </p>
+          <p>즉시이동http://kr.battle.net/heroes/ko/</p>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Let's Play
         </a>
       </header>
     </div>
